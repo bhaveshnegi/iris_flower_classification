a9bb45c654442094c7370636eaea1f646846a94a
bhaveshnegi <bhaveshnegi98@gmail.com>
2023-10-23 17:28:55 +0530

    Code updated

--- iris_flower_classification.py
+++ iris_flower_classification.py
@@ -1,7 +1,3 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt 
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
