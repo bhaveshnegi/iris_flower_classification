0d77b3462f4c87371bef936f47f216e41096de4c
bhaveshnegi <bhaveshnegi98@gmail.com>
2023-10-23 17:27:48 +0530

    Code updated

--- iris_flower_classification.py
+++ iris_flower_classification.py
@@ -25,13 +25,15 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.33,random_state=4
 #traning model
 
 knn = KNeighborsClassifier(n_neighbors = 3)
-knn.fit(X_train, y_train)
+print(knn.fit(X_train, y_train))
 
 #Evaluate model
 
 y_pred = knn.predict(X_test)
-print("y_pred",y_pred)
-print("y_test",y_test)
+print("y_pred")
+print(y_pred)
+print("y_test")
+print(y_test)
 
 #checkong accuracy
 
