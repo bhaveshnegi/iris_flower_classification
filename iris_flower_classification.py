c1f96c8d8bc85955c27f211511dbc0d770f0e4dc
bhaveshnegi <bhaveshnegi98@gmail.com>
2023-10-23 16:56:38 +0530

    data prepare

--- iris_flower_classification.py
+++ iris_flower_classification.py
@@ -7,3 +7,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score
 
 df=pd.read_csv('Iris.csv')
+df.drop('Id',axis=1,inplace=True)
+sp={'Iris-setosa':1,'Iris-versicolor':2,'Iris-virginica':3}
+df.Species=[sp[i] for i in df.Species]
+df
